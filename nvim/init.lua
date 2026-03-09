16a9d96a46631249ee416cad3f6cda1cf23b0abd
nmiller15 <nmiller@certifiedangusbeef.com>
2026-03-09 13:05:06 -0400

    fix roslyn/add psmux

--- nvim/init.lua
+++ nvim/init.lua
@@ -643,21 +643,53 @@ require('lazy').setup({
         postgres_lsp = {},
         -- roslyn = {
         --   on_attach = function()
-        --     print 'roslyn LSP attached'
+        --     print 'Roslyn LSP attached!'
         --   end,
-        --   filetypes = { 'cs', 'cshtml' },
+        --   cmd = (function()
+        --     if vim.fn.has 'win32' == 1 then
+        --       return {
+        --         'dotnet',
+        --         'C:/Program Files/roslyn/Microsoft.CodeAnalysis.LanguageServer.dll',
+        --         '--log-level=Information',
+        --         '--stdio',
+        --       }
+        --     end
+        --
+        --     return { 'roslyn' }
+        --   end)(),
         --   settings = {
+        --     ['csharp|background_analysis'] = {
+        --       dotnet_analyzer_diagnostics_scope = 'fullSollution',
+        --       dotnet_compiler_diagnostics_scope = 'fullSollution',
+        --     },
+        --     ['charp|code_lens'] = {
+        --       dotnet_enable_references_code_lens = true,
+        --       dotnet_enable_tests_code_lens = true,
+        --     },
+        --     ['csharp|completion'] = {
+        --       dotnet_provide_regex_completions = true,
+        --       dotnet_show_completion_items_from_unimported_namespaces = true,
+        --       dotnet_show_name_completion_suggestions = true,
+        --     },
         --     ['csharp|inlay_hints'] = {
         --       csharp_enable_inlay_hints_for_implicit_object_creation = true,
         --       csharp_enable_inlay_hints_for_implicit_variable_types = true,
+        --       csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+        --       csharp_enable_inlay_hints_for_types = true,
+        --       csharp_enable_inlay_hints_for_indexer_parameters = true,
+        --       csharp_enable_inlay_hints_for_literal_parameters = true,
+        --       dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+        --       dotnet_enable_inlay_hints_for_other_parameters = true,
+        --       dotnet_enable_inlay_hints_for_parameters = true,
+        --       dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+        --       dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+        --       dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
         --     },
-        --     ['csharp|background_analysis'] = {
-        --       dotnet_analyzer_diagnostics_scope = 'fullSolution',
-        --       dotnet_compiler_diagnostics_scope = 'fullSolution',
+        --     ['csharp|symbol_search'] = {
+        --       dotnet_search_reference_assemblies = true,
         --     },
-        --     ['csharp|code_lens'] = {
-        --       dotnet_enable_references_code_lens = true,
-        --       dotnet_enable_tests_code_lens = true,
+        --     ['csharp|formatting'] = {
+        --       dotnet_organize_imports_on_format = true,
         --     },
         --   },
         -- },
@@ -989,6 +1021,41 @@ require('lazy').setup({
         },
       }
     end,
+    settings = {
+      ['csharp|background_analysis'] = {
+        dotnet_analyzer_diagnostics_scope = 'fullSollution',
+        dotnet_compiler_diagnostics_scope = 'fullSollution',
+      },
+      ['charp|code_lens'] = {
+        dotnet_enable_references_code_lens = true,
+        dotnet_enable_tests_code_lens = true,
+      },
+      ['csharp|completion'] = {
+        dotnet_provide_regex_completions = true,
+        dotnet_show_completion_items_from_unimported_namespaces = true,
+        dotnet_show_name_completion_suggestions = true,
+      },
+      ['csharp|inlay_hints'] = {
+        csharp_enable_inlay_hints_for_implicit_object_creation = true,
+        csharp_enable_inlay_hints_for_implicit_variable_types = true,
+        csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+        csharp_enable_inlay_hints_for_types = true,
+        csharp_enable_inlay_hints_for_indexer_parameters = true,
+        csharp_enable_inlay_hints_for_literal_parameters = true,
+        dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+        dotnet_enable_inlay_hints_for_other_parameters = true,
+        dotnet_enable_inlay_hints_for_parameters = true,
+        dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+        dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+        dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+      },
+      ['csharp|symbol_search'] = {
+        dotnet_search_reference_assemblies = true,
+      },
+      ['csharp|formatting'] = {
+        dotnet_organize_imports_on_format = true,
+      },
+    },
     opts = {
       use_razor = true,
       -- your configuration comes here; leave empty for default settings
--- pwsh/keybind.ps1
+++ pwsh/keybind.ps1
@@ -4,4 +4,4 @@ Set-PSReadLineKeyHandler -Chord Ctrl+j -Function NextHistory
 Set-PSReadLineKeyHandler -Chord Ctrl+e -ScriptBlock { sf.ps1 }
 
 Set-PSReadLineKeyHandler -Chord Ctrl+a -ScriptBlock { reload 0 }
-Set-PSReadLineKeyHandler -Chord Ctrl+b -ScriptBlock { reload 1 }
+Set-PSReadLineKeyHandler -Chord Ctrl+s -ScriptBlock { reload 1 }
