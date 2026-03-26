194feb1ad46e2f56405a62f4d10ca8ad98a9f37f
nmiller15 <nmiller@certifiedangusbeef.com>
2026-03-26 14:40:11 -0400

    update keys

--- pwsh/keybind.ps1
+++ pwsh/keybind.ps1
@@ -5,3 +5,5 @@ Set-PSReadLineKeyHandler -Chord Ctrl+e -ScriptBlock { sf.ps1 }
 
 Set-PSReadLineKeyHandler -Chord Ctrl+a -ScriptBlock { reload 0 }
 Set-PSReadLineKeyHandler -Chord Ctrl+s -ScriptBlock { reload 1 }
+
+Set-PsReadLineKeyHandler -Chord Ctrl+n -ScriptBlock { . vault.ps1 }
