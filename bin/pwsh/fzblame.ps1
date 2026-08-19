5898763560b8d53b4fd7f95f563a3186e25ebd70
nmiller15 <nmiller@certifiedangusbeef.com>
2026-08-19 09:33:27 -0400

    update fzblame

--- bin/pwsh/fzblame.ps1
+++ bin/pwsh/fzblame.ps1
@@ -2,5 +2,5 @@ $selected = git ls-files | fzf --preview "bat --color=always --style=numbers {}"
 
 if ($selected.Length -gt 0)
 {
-    git blame $selected | less
+    git blame $selected --date=short | bat --color=always --language=log
 }
